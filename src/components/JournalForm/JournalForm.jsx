9258a57751e0b7d931444efc0f3207802a636978
anna_beliaeva <anna_beliaeva@list.ru>
2024-06-07 16:23:47 +0300

    feat: make Input component + forwardRef

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,15 +1,34 @@
 import Button from '../Button/Button'
-import { useEffect, useReducer, useState } from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
 import style from './JournalForm.module.css'
 import cn from 'classnames'
 import { INITIAL_STATE, formReducer } from './JournalForm.state'
+import Input from '../Input/Input'
 
 function JournalForm({ onSubmit }) {
   const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE)
   const { isFormValid, values, isFormReadyToSubmut } = formState
 
+  const headerRef = useRef()
+  const dateRef = useRef()
+  const postRef = useRef()
+
+  const focusError = (isFormValid) => {
+    switch (true) {
+      case !isFormValid.header:
+        headerRef.current.focus()
+        break
+      case !isFormValid.date:
+        dateRef.current.focus()
+        break
+      case !isFormValid.post:
+        postRef.current.focus()
+        break
+    }
+  }
   useEffect(() => {
     let timeoutId
+    focusError(isFormValid)
     if (!isFormValid.date || !isFormValid.header || !isFormValid.post) {
       timeoutId = setTimeout(() => {
         dispatchForm({ type: 'RESET_VALIDITY' })
@@ -42,29 +61,28 @@ function JournalForm({ onSubmit }) {
 
   return (
     <form className={style.journal_form} onSubmit={addJournalItem}>
-      <input
+      <Input
         type="text"
         name="header"
+        ref={headerRef}
         value={values.header}
         onChange={handleChange}
-        className={cn(style.input, style.header, {
-          [style.invalid]: !isFormValid.header
-        })}
+        appearance="header"
+        isValid={isFormValid.header}
       />
       <div className={style.form_row}>
         <label htmlFor="date" className={style.form_label}>
           <img src="/date.svg" alt="date" />
           <p>Дата</p>
         </label>
-        <input
+        <Input
           type="date"
           id="date"
           name="date"
+          ref={dateRef}
           value={values.date}
           onChange={handleChange}
-          className={cn(style.input, style.input, {
-            [style.invalid]: !isFormValid.date
-          })}
+          isValid={isFormValid.date}
         />
       </div>
       <div className={style.form_row}>
@@ -72,10 +90,9 @@ function JournalForm({ onSubmit }) {
           <img src="/tag.svg" alt="tag" />
           <p>Метки</p>
         </label>
-        <input
+        <Input
           type="text"
           id="tag"
-          className={style.input}
           value={values.tag}
           onChange={handleChange}
           name="tag"
@@ -86,6 +103,7 @@ function JournalForm({ onSubmit }) {
         cols="30"
         rows="10"
         value={values.post}
+        ref={postRef}
         onChange={handleChange}
         className={cn(style.input, style.post, {
           [style.invalid]: !isFormValid.post
